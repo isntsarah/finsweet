807e38af93c1eba95b853c4a8d5d3bf97b7572a2
isntsarah <sarahhosseini20030201@gmail.com>
2024-12-05 23:03:40 +0330

    finsweet

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+import"../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import"../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import"../css/main.css";
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./blog.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        custom: ["poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
